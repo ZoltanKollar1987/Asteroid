ec38a44bcf00bf7c45a0a39a724632570de82034
ZoltanKollar1987 <urza99@gmail.com>
2025-01-27 14:35:38 +0100

    Main Menu background music

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] AudioClip backGroundMusic;
+
+    private void Start()
+    {
+        SoundManager.Instance.PlayMusic(backGroundMusic);
+    }
     public void HowToPlay()
     {
         SceneManager.LoadScene("HowToPlay");
@@ -10,6 +16,7 @@ public class MenuManager : MonoBehaviour
     public void NewGame()
     {
         SceneManager.LoadScene("Game");
+        SoundManager.Instance.musicSource.Stop();
     }
     public void QuitGame()
     {
